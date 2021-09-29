37bf8e55de5e6790f41caced4c0e4bbaa3488aa3
Ruslan Gorbunov <fuldax17@gmail.com>
2021-09-29 18:08:53 +0300

    Dodel vse zonbI

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -12,17 +12,20 @@ int main()
 {
     float x, y;
     setlocale(LC_ALL, "RUS");
-    cout << "Введите координаты Х и Y";
-    cin >> x >> y;
+    cout << "Введите координаты Х: ";
+    cin >> x;
+    cout << "Введите координату Y: ";
+    cin >> y;
+
     //вычислим радиус-вектор точки
     float R = sqrt(x * x + y * y);
     int res = -1;
     
-
+    
     if (x < 0 && y >= 1.5) {
         res = 5;
     }
-
+    //формула элипса
     if ((4 * pow(y, 2) + pow(x, 2)) > 4) {
         if (x < -2 && y > 1 && y < 1.5) {
             res = 6;
@@ -30,15 +33,14 @@ int main()
      
 
     }
+    //формула круга
     else if (pow(x, 2) + pow(y, 2) > 1) {
         if (x < -1.5) {
             res = 20;
         }
-        else if (x > 1.5 && x < 0) {
-            res = 21;
-        }
+      
     }
-
+    //формула графика 3 степени 
     if (pow((x - 1), 3) > y) {
         if ((4 * pow(y, 2) + pow(x, 2)) > 4) {
             if (y < 1.5 && y > 0) {
@@ -50,13 +52,110 @@ int main()
 
         }
     }
-
+    //формула элипса
     if ((4 * pow(y, 2) + pow(x, 2)) > 4) {
         if (x > 0 && y < 0) {
             res = 1;
         }
     }
-    cout << res;
+
+    if (pow(x, 2) + pow(y, 2) < 1) {
+        if (x < 0 && y > 0) {
+            res = 21;
+        }
+        else if (x > 0 && y > 0) {
+            res = 14;
+        }
+
+        
+        
+        if (pow((x - 1), 3) > y) {
+            if (x > 0 && y < 0) {
+                res = 15;
+            }
+        }
+        else if (x > 0 && y < 0) {
+            res = 16;
+        }
+        
+    }
+
+    else if ((4 * pow(y, 2) + pow(x, 2)) < 4) {
+        if (pow((x - 1), 3) < y) {
+            if (x > 0 && y > 0) {
+
+                res = 12;
+            }
+
+        }
+        else if (x > 0 && y < 0) {
+            res = 10;
+        }
+    }
+    
+    
+    if (pow(x, 2) + pow(y, 2) < 1) {
+        if (0 - (sqrt(-x)) < y) {
+            if (x < 0 && y < 0) {
+                res = 22;
+            }
+        }
+        else if (x < 0 && y < 0) {
+            res = 17;
+        }
+    }
+    
+
+    if (pow(x, 2) + pow(y, 2) > 1) {
+        if ((4 * pow(y, 2) + pow(x, 2)) < 4) {
+            if (0 - (sqrt(-x)) > y) {
+                if (x < 0 && y < 0) {
+                    res = 18;
+                }
+            }
+            else if (x < 0 && y < 0) {
+                res = 19;
+            }
+        }
+
+
+    }
+
+    if ((4 * pow(y, 2) + pow(x, 2)) > 4) {
+        if (0 - (sqrt(-x)) > y) {
+            if (pow((x - 1), 3) > y) {
+                if (x < 0 && y < 0) {
+                    res = 9;
+                }
+
+            }
+            else if (x < 0 && y < 0) {
+                res = 8;
+            }
+        }
+        else if (x < 0 && y < 0) {
+            res = 7;
+        }
+        if (pow((x - 1), 3) < y) {
+            if (x > 0 && y > 0 && y < 1.5) {
+                res = 13;
+            }
+        }
+    }
+
+
+    if (pow((x - 1), 3) > y) {
+        if (x > 0 && y > 1.5) {
+            res = 3;
+        }
+    }
+    else if (x > 0 && y > 1.5) {
+        res = 4;
+    }
+
+
+
+    cout << "Вы ввели координаты участка № " << res;
          
 }   
 
